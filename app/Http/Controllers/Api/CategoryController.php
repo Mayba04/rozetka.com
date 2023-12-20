53d7f25b53de892eca80fda2eb849a166b8aeb6f
Pasha Mayba <pashamayba7@gmail.com>
2023-12-20 18:16:17 +0200

    Deleted and edited categories

--- app/Http/Controllers/Api/CategoryController.php
+++ app/Http/Controllers/Api/CategoryController.php
@@ -39,4 +39,64 @@ class CategoryController extends Controller
             ['Content-Type' => 'application/json;charset=UTF-8', 'Charset' => 'utf-8'],
             JSON_UNESCAPED_UNICODE);
     }
+
+    public function delete($id)
+    {
+        $category = Categories::find($id);
+
+        if ($category) {
+            $sizes = [50,150,300,600,1200];
+            foreach ($sizes as $size) {
+                $path = public_path("upload/".$size."_".$category->image);
+                if (file_exists($path)) {
+                    unlink($path);
+                }
+            }
+
+            $category->delete();
+
+            return response()->json('Category deleted successfully', 200);
+        } else {
+            return response()->json('Category not found', 404);
+        }
+    }
+
+    public function update(Request $request, $id)
+{
+    $category = Categories::find($id);
+    
+    if (!$category) {
+        return response()->json('Category not found', 404);
+    }
+
+    $input = $request->all();
+
+    if ($request->hasFile('image')) {
+        $sizes = [50,150,300,600,1200];
+        foreach ($sizes as $size) {
+            $oldPath = public_path("upload/".$size."_".$category->image);
+            if (file_exists($oldPath)) {
+                unlink($oldPath);
+            }
+        }
+
+        $image = $request->file("image");
+        $manager = new ImageManager(new Driver());
+        $imageName = uniqid().".webp";
+        
+        foreach ($sizes as $size) {
+            $imageSave = $manager->read($image);
+            $imageSave->scale(width: $size);
+            $path = public_path("upload/".$size."_" .$imageName);
+            $imageSave->toWebp()->save($path);
+        }
+
+        $input["image"] = $imageName;
+    }
+
+    $category->update($input);
+
+    return response()->json('Category updated successfully', 200);
+}
+
 }
--- routes/api.php
+++ routes/api.php
@@ -21,3 +21,5 @@ Route::middleware('auth:sanctum')->get('/user', function (Request $request) {
 
 Route::get('/categories', [CategoryController::class, 'getList']);
 Route::post('/categories', [CategoryController::class, 'create']);
+Route::delete('/categories/{id}', [CategoryController::class, 'delete']);
+Route::put('/categories/{id}', [CategoryController::class, 'update']);
